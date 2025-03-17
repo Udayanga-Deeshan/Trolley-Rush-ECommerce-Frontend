4b6cdc2b42780d88595ef4c51d1b3be2270a6e0d
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-03-18 01:40:24 +0530

    implemented routes for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./common/header/header.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { AboutUsComponent } from "./pages/about-us/about-us.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
